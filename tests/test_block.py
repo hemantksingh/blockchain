4a54143859910b58f1140cfad7735d3ec0e41601
hemantksingh <hemantksingh.hk@gmail.com>
2017-09-08 00:36:15 +0100

    Adds previous hash validation.

--- tests/test_block.py
+++ tests/test_block.py
@@ -18,15 +18,17 @@ class Block(object):
     def has_valid_hash(self):
         return self.hash == self.calc_hash()
 
+    def has_valid_previous_hash(self, prev_block):
+        return self.previous_hash == prev_block.hash
+
     def calc_hash(self):
         return hashlib.sha256(str(self).encode()).hexdigest()
-
 
 class TestBlockTest(unittest.TestCase):
 
     def test_valid_index(self):
-        prev_block = Block(0, '0', 1, 'first block')
-        block = Block(1, '0', 1, 'second block')
+        prev_block = Block(index=0, previous_hash='0', timestamp=1, data='first block')
+        block = Block(index=1, previous_hash='0', timestamp=1, data='second block')
         self.assertTrue(block.has_valid_index(prev_block))
 
     def test_invalid_index(self):
@@ -34,9 +36,19 @@ class TestBlockTest(unittest.TestCase):
         block = Block(2, '0', 1, 'second block')
         self.assertFalse(block.has_valid_index(prev_block))
 
-    def test_has_valid_hash(self):
+    def test_valid_hash(self):
         block = Block(1, '0', '0', 'first block')
         self.assertTrue(block.has_valid_hash())
+    
+    def test_valid_previous_hash(self):
+        prev_block = Block(index=0, previous_hash='0', timestamp=1, data='first block')
+        block = Block(index=1, previous_hash=prev_block.hash, timestamp=1, data='second block')
+        self.assertTrue(block.has_valid_previous_hash(prev_block))
+
+    def test_invalid_previous_hash(self):
+        prev_block = Block(index=0, previous_hash='0', timestamp=1, data='first block')
+        block = Block(index=1, previous_hash='1', timestamp=1, data='second block')
+        self.assertFalse(block.has_valid_previous_hash(prev_block))
 
 if __name__ == '__main__':
     unittest.main()
