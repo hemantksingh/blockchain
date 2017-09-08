59c6347898d7a8a5fb93013101cc23868088ff25
hemantksingh <hemantksingh.hk@gmail.com>
2017-09-08 17:41:18 +0100

    Adds block builder

--- tests/test_block.py
+++ tests/test_block.py
@@ -5,29 +5,52 @@ sys.path.append('src')
 from block import Block
 from blockchain import BlockChain
 
+class BlockBuilder(object):
+    def __init__(self):
+        self._index = 0
+        self._previous_hash='0'
+        self._timestamp=int (time.time())
+        self._data='first block'
+
+    def index(self, index):
+        self._index=index
+        return self
+
+    def data(self, data):
+        self._data=data
+        return self
+
+    def build(self):
+        return Block(
+            index=self._index,
+            previous_hash=self._previous_hash,
+            timestamp=self._timestamp,
+            data=self._data
+        )
+
 class TestBlockTest(unittest.TestCase):
 
     def test_valid_index(self):
-        prev_block = Block(index=0, previous_hash='0', timestamp=1, data='first block')
-        block = Block(index=1, previous_hash='0', timestamp=1, data='second block')
+        prev_block = BlockBuilder().index(0).build()
+        block = BlockBuilder().index(1).build()
         self.assertTrue(block.has_valid_index(prev_block))
 
     def test_invalid_index(self):
-        prev_block = Block(0, '0', 1, 'first block')
-        block = Block(2, '0', 1, 'second block')
+        prev_block = BlockBuilder().index(0).build()
+        block = BlockBuilder().index(2).build()
         self.assertFalse(block.has_valid_index(prev_block))
 
     def test_valid_hash(self):
-        block = Block(1, '0', '0', 'first block')
+        block = BlockBuilder().build()
         self.assertTrue(block.has_valid_hash())
 
     def test_valid_previous_hash(self):
-        prev_block = Block(index=0, previous_hash='0', timestamp=1, data='first block')
+        prev_block = BlockBuilder().build()
         block = Block(index=1, previous_hash=prev_block.hash, timestamp=1, data='second block')
         self.assertTrue(block.has_valid_previous_hash(prev_block))
 
     def test_invalid_previous_hash(self):
-        prev_block = Block(index=0, previous_hash='0', timestamp=1, data='first block')
+        prev_block = BlockBuilder().build()
         block = Block(index=1, previous_hash='1', timestamp=1, data='second block')
         self.assertFalse(block.has_valid_previous_hash(prev_block))
 
